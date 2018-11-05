d7be8a9bb7e154c480ec262421f5bf118e16f72d
Rostyslav Bortman <bortmanrostyslav@gmail.com>
2018-11-05 11:49:51 +0200

    AgreementContract

---
+++ contracts/AgreementContract.sol
@@ -0,0 +1,65 @@
+pragma solidity ^0.4.25;
+
+// Copyright 2017 Bittrex
+
+contract AgreementContract {
+
+    enum AgreementStatus { New, Signed, Declined }
+    AgreementStatus status;
+
+    string public agreementDetails;
+    string public declineReason;
+    address public tenant;
+    address public landlord;
+    bool signedByTenant;
+    bool signedByLandlord;
+
+    event AgreementCreated(address tenant, address landlord);
+    event AgreementSigned(address by);
+    event AgreementDeclined(address by, string reason);
+
+    modifier onlyTenantOrLandlord() { 
+        require(msg.sende r== tenant || msg.sender == landlord); 
+        _; 
+    }
+
+    modifier onlyNew() { 
+        require(status == AgreementStatus.New); 
+        _; 
+    }
+    
+    constructor (address _tenant, address _landlord ,string _agreementDetails){
+        tenant = _tenant;
+        landlord = _landlord;
+        agreementDetails = _agreementDetails;
+        status = AgreementStatus.New;
+        emit AgreementCreated(_tenant, _landlord);
+    }
+
+    function signAgreement() onlyTenantOrLandlord public returns(bool){
+        if(msg.sender == tenant){
+            require(signedByTenant == false);
+            signedByTenant = true;
+            emit AgreementSigned(msg.sender);
+            if(signedByLandlord == true){
+                status = AgreementStatus.Signed;
+            }
+        } else {
+            require(signedByLandlord == false);
+            signedByLandlord = true;
+            emit AgreementSigned(msg.sender);
+            if(signedByTenant == true){
+                status = AgreementStatus.Signed;
+            }
+        }
+    }
+
+    function declineAgreement(string _reason) onlyTenantOrLandlord onlyNew public returns(bool){
+        require (status!=AgreementStatus.Declined);
+        
+        declineReason = _reason;
+        status = AgreementStatus.Declined;
+        emit AgreementDeclined(msg.sender, _reason);
+    }
+    
+}
